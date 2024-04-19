a68d5ab11e597d48f1a6ff71730ece94d89a9374
dasv21 <davidsantiago434@gmail.com>
2024-04-19 13:35:43 -0400

    feature: MSVC-Authentication MSVC-Gateway se agrego el AuthenticationFilter a los endpoinds

---
+++ msvc-gateway/src/main/java/com/msvc/gateway/util/JwtUtil.java
@@ -0,0 +1,26 @@
+package com.msvc.gateway.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Component
+public class JwtUtil {
+
+    public static final String SECRET_KEY = "7493763542GV59MK3H1L089479HLJL0HJKL21KL2J73456321L786506J89008530927856624";
+
+
+    public void validateToken(final String token) {
+        Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
+    }
+
+
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
